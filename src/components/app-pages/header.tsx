9aa4c136ce166abcc51217794d4d11f9dacabc4b
Mongiwa Ndebele <mongiwandebele@Mongi-at-Pineapples-MacBook-Air.local>
2024-08-02 15:16:05 +0200

    improvedstickywall

--- src/components/app-pages/header.tsx
+++ src/components/app-pages/header.tsx
@@ -5,11 +5,11 @@ import styles from '../../sass/app-pages/header.module.sass'
 const Header = ({ activeScreen, handleShow, isShifted }) => {
   return (
     <div className={`d-flex ${styles.header} ${isShifted ? styles.shifted : ""}`}>
-      <button onClick={handleShow} className={styles.burgerbutton}>
-        <TfiMenu size={30}/>
+      <button onClick={handleShow} className={`${styles.burgerbutton} ${isShifted ? styles.shiftburger: ""} `}>
+        <TfiMenu size={20}/>
       </button>
       <div>
-      <h1 className={styles.screenheading}>{activeScreen}</h1>
+      <h1 className={`${styles.screenheading} `}>{activeScreen}</h1>
       </div>
     </div>
   );
